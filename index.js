12ee4e5c1ef073e9e26ff1db1c95a65f751f64be
yurashk <Yuriy.Gienko@fuib.com>
2022-06-13 00:00:08 +0300

    13 commit.

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use("/app", appRouter)
 app.use(express.static(path.join(__dirname, 'public')));
 
 const sslServer=https.createServer({
-    NODE_TLS_REJECT_UNAUTHORIZED : '0',
+    rejectUnauthorized: false,
     key:fs.readFileSync(path.join(__dirname,'cert','key.pem')),
     cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
 },app)
