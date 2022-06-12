d7f412f92f254f3431d7ccce76afc304d2f5b567
yurashk <Yuriy.Gienko@fuib.com>
2022-06-13 00:26:47 +0300

    15 commit.

--- index.js
+++ index.js
@@ -1,31 +1,33 @@
-
 const express = require("express")
 const mongoose = require("mongoose")
 // const config = require("config")
-const https=require('https')
-const fs=require('fs')
+const https = require('https')
+const fs = require('fs')
 const authRouter = require("./routers/authRouter")
 const appRouter = require("./routers/appRouter")
 const app = express()
 const PORT = process.env.PORT || 5000
 const corsMiddleware = require("./middleware/cors.middleware")
-const path=require('path')
+const path = require('path')
 app.use(corsMiddleware)
 app.use(express.json())
 app.use("/auth", authRouter)
 app.use("/app", appRouter)
 app.use(express.static(path.join(__dirname, 'public')));
 
-const sslServer=https.createServer({
+const sslServer = https.createServer({
     rejectUnauthorized: false,
-    key:fs.readFileSync(path.join(__dirname,'cert','key.pem')),
-    cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
-},app)
+    // key:fs.readFileSync(path.join(__dirname,'cert','key.pem')),
+    // cert:fs.readFileSync(path.join(__dirname,'cert','cert.pem'))
+    key: fs.readFileSync(path.join(__dirname, 'cert', 'server-key.pem')),
+    cert: fs.readFileSync(path.join(__dirname, 'cert', 'server-crt.pem')),
+    ca: fs.readFileSync(path.join(__dirname, 'cert', 'ca-crt.pem')),
+}, app)
 
 const start = async () => {
     try {
         await mongoose.connect(`mongodb+srv://yurashk:92Ozuzud@cluster0.cobjhb1.mongodb.net/?retryWrites=true&w=majority`)
-        sslServer.listen(5000,()=> console.log(`server started at ${PORT} port`))
+        sslServer.listen(5000, () => console.log(`server started at ${PORT} port`))
         // app.listen(PORT, () => console.log(`server started at ${PORT} port`))
     } catch (e) {
         console.log(e)
