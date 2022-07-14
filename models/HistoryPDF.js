ba6b1cc4890df652d8ebceec76ad387a2ec7ec3b
yurashk <Yuriy.Gienko@fuib.com>
2022-07-14 16:44:50 +0300

    add pdf history commit.

--- models/HistoryPDF.js
+++ models/HistoryPDF.js
@@ -1,7 +1,7 @@
 const {Schema, model} = require("mongoose")
 
 const HistoryPDF = new Schema({
-    pdfUrl: {type: String, unique: true, required: true},
+    pdfUrl: {type: String,  required: true},
     ownerId: {type: String, required: true},
     dateOfCreation: {type: String, required: true},
     mileage: {type: String, required: true},
